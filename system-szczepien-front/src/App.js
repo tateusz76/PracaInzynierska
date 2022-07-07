418b1f8d61e62b71bf719f651fc568e7040bea71
Mateusz <traczyk.mateusz76@gmail.com>
2022-07-07 09:57:13 +0200

    Start frontu

---
+++ system-szczepien-front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PatientProfile from './Flow/PatientProfile/PatientProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <PatientProfile/> 
+    </div>
+  );
+}
+
+export default App;
